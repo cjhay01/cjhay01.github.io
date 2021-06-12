75e4b7c5daf4a63a133d9a87d04eebdc8a445733
cjhay01 <cj01baniqued@gmail.com>
2021-06-12 19:27:12 +0800

    h

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+var cursorCount = 0;
+var cursorCost = 0;
 var score = 0;
     function increase() {
       score++;
       document.getElementById('counter').innerHTML = score;
+    }
+    document.getElementById("buyCursorBtn").onclick = function() {
+        if (score >= cursorCost) {
+            score -= cursorCost;
+            cursorCount++;
+        }
     }
