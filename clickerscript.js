edf3f70b10aff750ba11aad2363cdeeeb88a817e
cjhay <71071772+cjhay01@users.noreply.github.com>
2022-06-01 23:41:33 +0800

    Add files via upload

--- clickerscript.js
+++ clickerscript.js
@@ -21,12 +21,14 @@ cursorCost: 100,
 cpc: 1,
 cpcCost: 69,
 gen2Count: 0,
-gen2Cost: 0,
+gen2Cost: 5000,
 }
 if (savegame !== null) gameData = savegame;
 document.getElementById("cursorCost").innerHTML = gameData.cursorCost;
 document.getElementById("cpcCost").innerHTML = gameData.cpcCost;
 document.getElementById("cursorCount").innerHTML = gameData.cursorCount;
+document.getElementById("gen2Count").innerHTML = gameData.gen2Count;
+document.getElementById("gen2Cost").innerHTML = gameData.gen2Cost;
 function increase() {
     gameData.score += gameData.cpc;
     document.getElementById('counter').innerHTML = Math.round(gameData.score).toLocaleString();
@@ -36,19 +38,32 @@ function increase() {
             gameData.score -= gameData.cursorCost;
             gameData.cursorCount++;
             gameData.cps++;
-            gameData.cursorCost = Math.ceil(Math.pow(gameData.cursorCost, 1.00));
+            gameData.cursorCost = Math.floor(gameData.cursorCost + gameData.cursorCost * 0);
             document.getElementById('cursorCost').innerHTML = gameData.cursorCost.toLocaleString();
             cursorCountElement.innerHTML = gameData.cursorCount;
         }
     }
-    document.getElementById("cpcBtn").onclick = function() {
+    document.getElementById("gen2Btn").onclick = function() {
+        if (gameData.score >= gameData.gen2Cost) {
+            gameData.score -= gameData.gen2Cost;
+            gameData.gen2Count++;
+            gameData.cps += 100;
+            gameData.gen2Cost = Math.ceil(Math.pow(gameData.gen2Cost, 1.00));
+            document.getElementById('gen2Cost').innerHTML = gameData.gen2Cost.toLocaleString();
+            document.getElementById('gen2Count').innerHTML = gameData.gen2Count;
+        }
+    }
+    function cpcPurchase() {
         if (gameData.score >= gameData.cpcCost) {
             gameData.score -= gameData.cpcCost;
             gameData.cpc++;
-            gameData.cpcCost = Math.ceil(Math.pow(gameData.cpcCost, 1.02));
+            gameData.cpcCost = Math.ceil(Math.pow(gameData.cpcCost, 1.00));
             document.getElementById('cpcCost').innerHTML = gameData.cpcCost.toLocaleString()
             cpcElement.innerHTML = gameData.cpc;
         }
+    }
+    document.getElementById("cpcMax").onclick = function() {
+        while (gameData.score >= gameData.cpcCost) cpcPurchase()
     }
     cpcElement = document.getElementById("cpc");
         cpcElement.innerHTML = gameData.cpc;
@@ -56,7 +71,7 @@ function increase() {
         setInterval(function(){
             gameData.score += gameData.cps * 0.004;
             document.getElementById('counter').innerHTML = Math.round(gameData.score).toLocaleString();
-            document.getElementById("scorePerSec").innerHTML = gameData.cps
+            document.getElementById('scorePerSec').innerHTML = gameData.cps
         }, 1);
 var saveGameLoop = window.setInterval(function() {
     localStorage.setItem("gameSave", JSON.stringify(gameData))
@@ -64,7 +79,7 @@ var saveGameLoop = window.setInterval(function() {
 function save() {
     localStorage.setItem("gameSave", JSON.stringify(gameData))
 }
-function myFunction() {
+function dm() {
     var element = document.body;
     element.classList.toggle("dark-mode");
     if (element.classList.contains("dark-mode")) {
